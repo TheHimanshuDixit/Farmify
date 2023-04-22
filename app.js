d48189aaea1bbeb996d6866f95dfc74531f69039
91aashutosh <aashu2348154@gmail.com>
2023-04-23 00:33:17 +0530

    template-folder-added-companyDashboard-Template-added

--- app.js
+++ app.js
@@ -117,6 +117,7 @@ app.post("/loginfarmer", async (req, res) => {
 app.get("/signup", function (req, res) {
   res.render("signup");
 });
+
 app.post("/registercompany", async (req, res) => {
   const config = {
     headers: {
